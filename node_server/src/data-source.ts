4f969dab06a595449a3af88555a247feb302e5fc
= <nsthien2109@gmail.com>
2023-12-18 00:11:01 +0700

    updaete error map

--- node_server/src/data-source.ts
+++ node_server/src/data-source.ts
@@ -6,11 +6,11 @@ import { Bird, History, Prediction } from "./entity";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: "localhost",
+  host: "34.143.244.24",
   port: 3306,
   username: "root",
   password: "Bame12345@",
-  database: "njha-api",
+  database: "njha_bird_db",
   synchronize: true,
   logging: false,
   entities: [User, Role, History, Prediction, Bird],
